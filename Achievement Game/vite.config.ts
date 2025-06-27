8fd85cab966d49297616f7d539b07b6b244f7db5
Zhu Yangyi <142135488+yangyi-zhu@users.noreply.github.com>
2025-06-28 04:02:10 +0800

    Fix Vite config host TypeError

--- Achievement Game/vite.config.ts
+++ Achievement Game/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // @ts-expect-error process is a nodejs global
-const host = process.env.TAURI_DEV_HOST;
+const tauriDevHost = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -16,11 +16,11 @@ export default defineConfig(async () => ({
   server: {
     port: 1420,
     strictPort: true,
-    host: host || false,
-    hmr: host
+    host: tauriDevHost,
+    hmr: tauriDevHost
       ? {
           protocol: "ws",
-          host,
+          host: tauriDevHost,
           port: 1421,
         }
       : undefined,
